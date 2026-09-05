aa0353ba156b2a1777527df0c06f8b9db756375c
EPS-TOPIK Admin <admin@epstopik.app>
2026-09-05 09:50:45 +0545

    feat: link in-app APK download popup directly to new online-sync APK in repository

--- main.dart.js
+++ main.dart.js
@@ -33161,8 +33161,8 @@ for(s=A.cN(a,a.r,A.r(a).c),r=s.$ti.c;s.A();){q=s.d
 if(q==null)q=r.a(q)
 if(b.$1(q))return q}return null},
 b9K(){var s,r=document,q=r.createElement("a")
-q.href="https://github.com/brosandy9999/ubttest2026/releases/download/v1.0.0/EPS-TOPIK.apk"
-q.setAttribute("download","EPS-TOPIK-UBT-Practice.apk")
+q.href="https://github.com/brosandy9999/epstopikabante2072/raw/main/apk/eps_topik_ubt_2026.apk"
+q.setAttribute("download","eps_topik_ubt_2026.apk")
 q.setAttribute("target","_blank")
 s=q.style
 s.display="none"
